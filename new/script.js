afcd33841b1cb2742debc7684105c950c2552b94
funnsam <61924648+funnsam@users.noreply.github.com>
2022-08-31 13:57:34 +0800

    Add files via upload

---
+++ new/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    main();
+ }, false);
+
+function main() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    if (urlParams.has('url')) {
+        document.getElementById("seecrets").value = "funnsam.github.io/seecrets?l="+btoa(urlParams.get('url'));
+    }
+}
---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    main();
+ }, false);
+
+function main() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    if (urlParams.has('l') == false || urlParams.get('l') == '') {
+        window.location.replace('new/');
+    }
+
+    let URLString = atob(urlParams.get('l'));
+
+    try {
+        _ = new URL(URLString);
+    } catch (err) {
+        window.alert(err);
+    }
+    
+    window.location.replace(URLString);
+}
